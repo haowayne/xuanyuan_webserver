2bbe5e039507f6c16304d15f35e51d2e32a35b71
haowayne <waynehao@ebupt.edu.cn>
2021-12-06 05:12:50 -0800

    day5

--- day5/main.c
+++ day5/main.c
@@ -17,13 +17,14 @@ int main() {
                    "</html>";
     char buf[512] = {0};
     char send_buf[512] = {0};
+    char file_buf[512] = {0};
     FILE *fd = NULL;
     int serv_sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     struct sockaddr_in serv_addr;
     memset(&serv_addr,0,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
-    serv_addr.sin_port = htons(9999);
+    serv_addr.sin_port = htons(10007);
     bind(serv_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
     listen(serv_sock,20);
     struct sockaddr_in clnt_addr;
@@ -33,25 +34,28 @@ int main() {
     {
         clnt_sockt = accept(serv_sock,(struct sockaddr*)&clnt_addr,&clnt_addr_size);
         recv(clnt_sockt,buf,512,0);
-        if(strncmp(buf,"GET / HTTP/1.1\r\n",18))
+        if(!strncmp(buf,"GET / HTTP/1.1",14))
         {
-            if(NULL == (fd = fopen("index1.html","r")))
+            if(NULL == (fd = fopen("../day5/index.html","r")))
             {
                 strcat(send_buf,response_head);
                 strcat(send_buf,content);
                 write(clnt_sockt,send_buf, sizeof(send_buf));
                 memset(send_buf,0, sizeof(send_buf));
                 close(clnt_sockt);
+                printf("send code finish\n");
                 continue;
             }
-            fgets(content,sizeof(send_buf),fd);
             strcat(send_buf,response_head);
-            strcat(send_buf,content);
+            while(fgets(file_buf,sizeof(send_buf),fd))
+                strcat(send_buf,file_buf);
             write(clnt_sockt,send_buf, sizeof(send_buf));
             memset(send_buf,0, sizeof(send_buf));
             close(clnt_sockt);
+            printf("send file finish\n");
             continue;
         }
     }
+    close(serv_sock);
     return 0;
 }
